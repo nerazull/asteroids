fd8578b904afc3fc2f2ade91c00dae4ff9b6cc73
nerazull <dragosnboston@gmail.com>
2026-01-13 13:35:57 +0200

    Add groups to main.py, switch to calling groups instead of Player object

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     clock = pygame.time.Clock()
@@ -24,8 +29,11 @@ def main():
                 return
             
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for item in drawable:
+            item.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
